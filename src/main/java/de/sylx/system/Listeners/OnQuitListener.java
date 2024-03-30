72b33eb101c1710cc806887841814034678948dd
sylx <scopter10leon@gmail.com>
2024-03-30 04:19:58 +0100

    Added Sound to QuitListener

--- src/main/java/de/sylx/system/Listeners/OnQuitListener.java
+++ src/main/java/de/sylx/system/Listeners/OnQuitListener.java
@@ -1,5 +1,6 @@
 package de.sylx.system.Listeners;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,5 +12,8 @@ public class OnQuitListener implements Listener {
         Player p = e.getPlayer();
 
         e.setQuitMessage("§c" + p.getName() + " hat das Spiel Verlassen der huransohn");
+
+        p.playSound(p.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_0, 1.0f, 1.0f);
+
     }
 }
